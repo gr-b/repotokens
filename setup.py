8723f5ad33f2f1a06e61f13550770c95d80cc034
gr-b <griffin.bishop24@gmail.com>
2024-09-06 19:20:57 -0400

    Actually publish readme

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name='repotokens',
-    version='1.0.0',
+    version='1.0.1',
     description="Count number of LLM tokens of source files contained in the repository / folder",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     py_modules=['repotokens'],
     install_requires=[
         'tiktoken',
