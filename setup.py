1dbb60e876e2f7b72cd043508f1b2cf99e43d9f8
gr-b <griffin.bishop24@gmail.com>
2024-09-06 19:12:38 -0400

    Add description / manifest

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='repotokens',
     version='1.0.0',
+    description="Count number of LLM tokens of source files contained in the repository / folder",
     py_modules=['repotokens'],
     install_requires=[
         'tiktoken',
